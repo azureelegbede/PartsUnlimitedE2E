4a2b522803584ac5789dd505fbb14f4adde0fb04
Uriel ELEGBEDE <uriel.elegbede@gmail.com>
2020-07-27 10:21:38 +0200

    Mycommit2

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // My fisrt change.
+        // My fourth change.
         [Key]
         public int CartItemId { get; set; }
 
